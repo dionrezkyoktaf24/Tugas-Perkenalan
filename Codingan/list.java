c1946a7e710589acb3fdbc4176565a2e9b7bd863
dionrezkyoktaf24 <dion_oktafino_33rpl@student.smktelkom-mlg.sch.id>
2024-08-01 06:19:39 +0700

    Add files via upload

---
+++ Codingan/list.java
@@ -0,0 +1,11 @@
+public class list {
+    public static void main(String[] args) {
+        String nama = "Nama : Dion Rezky OktaFino";
+        String alamat = "Alamat : Bendo - Kepanjen Kidul, Kota Blitar";
+        String smp = "Asal SMP : SMP Negeri 3 Kota Blitar";
+        String nohp = "No. hp : 085791360267";
+
+        System.out.println(nama + System.lineSeparator() + alamat + System.lineSeparator() + smp + System.lineSeparator()+nohp);
+    }
+    
+}
